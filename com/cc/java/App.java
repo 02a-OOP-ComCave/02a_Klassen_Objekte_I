6ac45a57a2bfed76c8cb5132684bf90b7b2c6bf4
Peter <schmidt.multimedia@gmail.com>
2025-01-16 11:07:45 +0100

    2. Iteration / Instanziierung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,10 +4,18 @@ public class App {
     
     public static void main(String[] args) {
 
-        /* 1. Iteration: Objekte durch new erzeugen */ 
+      /*
+       *    2. Iteration
+       *    Instanziierung  --> Erzeugen eines Objektes
+       *    Objekt <---> Instanz
+       *    cat --> Referenzvariable
+       *    Cat --> Referenztyp(Klasse)
+       * 
+       */
 
-        System.out.println(new Cat());
-        System.out.println(new Cat());
+        Cat cat = new Cat();
+        System.out.println(cat);
+
 
     }
 
